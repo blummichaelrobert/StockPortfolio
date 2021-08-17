5e4c8d04e13593430432c95732c445980bd2d882
Michael Blum <Michael.Blum@webpt.com>
2021-08-16 17:53:45 -0700

    initial commit

---
+++ AngularDemo/Repository/IOLD_Repository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularDemo.Repo
+{
+    public interface IOLD_Repository
+    {
+        Task<List<T>> SelectAll<T>() where T : class;
+        Task<T> SelectById<T>(long id) where T : class;
+        Task CreateAsync<T>(T entity) where T : class;
+        Task UpdateAsync<T>(T entity) where T : class;
+        Task DeleteAsync<T>(T entity) where T : class;
+    }
+}
---
+++ AngularDemo/Repository/MemberRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AngularDemo.Models;
+
+namespace AngularDemo.Repository
+{
+    public class MemberRepository : DbContext
+    {
+        private readonly AppDBContext context;
+
+        public MemberRepository(AppDBContext context)
+        {
+            this.context = context;
+        }
+    }
+}
